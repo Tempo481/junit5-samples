d9b6b4b2a2ee33eb855f4fa92187578cca134a98
Matthias Adrian <matthiasadrian7@gmail.com>
2021-09-10 18:30:48 +0200

    added database mock

---
+++ junit5-jupiter-starter-maven/src/main/java/com/example/project/Database.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+public class Database {
+
+    public boolean isAvailable() {
+        // currently not implemented, as this is just demo used in a software test
+        return false;
+    }
+    public int getUniqueId() {
+        return 42;
+    }
+    
+}
---
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/ServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.project;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+class ServiceTest {
+
+    @Mock
+    Database databaseMock;                                  
+
+    @Test
+    public void testQuery()  {
+        assertNotNull(databaseMock);
+        when(databaseMock.isAvailable()).thenReturn(true);  
+        Service t  = new Service(databaseMock);             
+        boolean check = t.query("* from t");                
+        assertTrue(check);
+    }    
+    
+}
