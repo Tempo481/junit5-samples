c3cebaa76e85c2670ca7ac78813b25b6010aff76
Tempo481 <matthiasadrian7@gmail.com>
2021-09-13 03:24:11 +0200

    fixed test by adding mockito-junit dependency

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/ServiceTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/ServiceTest.java
@@ -6,7 +6,9 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)                         
 class ServiceTest {
 
     @Mock
