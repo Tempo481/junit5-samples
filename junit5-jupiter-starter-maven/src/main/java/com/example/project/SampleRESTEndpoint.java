27994622c586de1735941794888b7efed9085fe7
Matthias Adrian <matthiasadrian7@gmail.com>
2021-09-16 18:26:21 +0200

    samplerestendpoint test

--- junit5-jupiter-starter-maven/src/main/java/com/example/project/SampleRESTEndpoint.java
+++ junit5-jupiter-starter-maven/src/main/java/com/example/project/SampleRESTEndpoint.java
@@ -1,11 +1,9 @@
 package com.example.project;
 
 public class SampleRESTEndpoint {
-	
-	
+		
 	public String getHTTPGetData() {
 		return "this would return data from a database";
 	}
-
 	
 }
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/SampleRESTEndpointTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/SampleRESTEndpointTest.java
@@ -1,5 +1,19 @@
 package com.example.project;
 
+import java.net.http.HttpResponse;
+
+import org.junit.jupiter.api.Test;
+
 public class SampleRESTEndpointTest {
 
+	
+	@Test
+	public void getSampleRESTEndpointTest() {
+		
+		
+		
+	}
+	
+	
+	
 }
