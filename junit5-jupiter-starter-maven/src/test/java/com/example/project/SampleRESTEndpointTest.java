788df553744d88192088bdf66e7fa4656f13b7b8
Tempo481 <matthiasadrian7@gmail.com>
2021-09-17 04:04:09 +0200

    Update SampleRESTEndpointTest.java

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/SampleRESTEndpointTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/SampleRESTEndpointTest.java
@@ -3,6 +3,7 @@ package com.example.project;
 import java.net.http.HttpResponse;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class SampleRESTEndpointTest {
 
@@ -10,7 +11,7 @@ public class SampleRESTEndpointTest {
 	@Test
 	public void getSampleRESTEndpointTest() {
 		
-		
+		HttpResponse<String> mockedResponse = Mockito.mock(HttpResponse.class);
 		
 	}
 	
